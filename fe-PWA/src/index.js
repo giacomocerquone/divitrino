ba1ab1473de43d9b92f3e55c63d10440dc8e491f
gianlucadifrancesco <genius.96@hotmail.it>
2020-12-15 13:38:45 +0100

    Minor fix

--- fe-PWA/src/index.js
+++ fe-PWA/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { defineCustomElements } from "@ionic/pwa-elements/loader";
 import Dinero from "dinero.js";
 
 /* Core CSS required for Ionic components to work properly */
@@ -25,8 +24,6 @@ import "./theme/variables.css";
 Dinero.defaultCurrency = "EUR";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-defineCustomElements(window);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
