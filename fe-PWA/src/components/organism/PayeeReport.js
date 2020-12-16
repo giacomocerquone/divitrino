4c216ad65439bc3900dc8058943d5aac4980c4d6
Giacomo Cerquone <cerquone96@hotmail.it>
2020-12-16 09:32:46 +0100

    chore: linting

--- fe-PWA/src/components/organism/PayeeReport.js
+++ fe-PWA/src/components/organism/PayeeReport.js
@@ -8,35 +8,36 @@ import {
 import { useSelector } from "react-redux";
 import { getPeople, getDebits } from "store/app.reducer";
 
-const SingleReport = ({p, isPayer = false}) => {
+const SingleReport = ({ p, isPayer = false }) => {
   const people = useSelector(getPeople);
   const debits = useSelector((state) => getDebits(state));
 
-  const result = people.map((person) => {
-      if (person.id === p.id) return;
-      
+  const result = people
+    .map((person) => {
+      if (person.id === p.id) return "";
+
       const dinero = isPayer
         ? debits[p.id][person.id]
         : debits[person.id][p.id];
-      if (dinero.getAmount() <= 0) return; 
+      if (dinero.getAmount() <= 0) return "";
 
       return (
-        <div key={person.id}>
-          <span>
-            - {isPayer ? 'a' : 'da'} <b>{person.name}</b>: {dinero.toFormat('$0,0.00')}
-          </span>
-        </div>
+        <p key={person.id}>
+          - {isPayer ? "a" : "da"} <b>{person.name}</b>:{" "}
+          {dinero.toFormat("$0,0.00")}
+        </p>
       );
-    }).filter((el) => !!el);
+    })
+    .filter((el) => !!el);
 
-  return result.length ? result : (
-    <small style={{opacity: 0.6}}>- Vuoto -</small> 
+  return result.length ? (
+    result
+  ) : (
+    <small style={{ opacity: 0.6 }}>- Vuoto -</small>
   );
 };
 
 const PayeeReport = ({ person }) => {
-  const people = useSelector(getPeople);
-
   return (
     <IonCard mode="ios">
       <IonCardContent>
@@ -44,7 +45,9 @@ const PayeeReport = ({ person }) => {
         <IonCardSubtitle style={{ marginTop: 10 }}>deve dare</IonCardSubtitle>
         <SingleReport p={person} isPayer />
 
-        <IonCardSubtitle style={{ marginTop: 10 }}>deve ricevere</IonCardSubtitle>
+        <IonCardSubtitle style={{ marginTop: 10 }}>
+          deve ricevere
+        </IonCardSubtitle>
         <SingleReport p={person} />
       </IonCardContent>
     </IonCard>
