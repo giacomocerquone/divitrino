5d48a84cf709884cb74c5b0dd34a3e2a20e029a4
Giacomo Cerquone <cerquone96@hotmail.it>
2020-12-18 02:45:57 +0100

    built reusable hooks for prods inputs

--- fe/src/components/organism/AddPurchase/NewProdRow.js
+++ fe/src/components/organism/AddPurchase/NewProdRow.js
@@ -1,27 +1,44 @@
-import { IonButton, IonIcon, IonInput, IonItem } from "@ionic/react";
+import { IonButton, IonIcon, IonItem } from "@ionic/react";
 import { add } from "ionicons/icons";
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import styled from "styled-components";
+import useProdInputs from "hooks/useProdInputs";
 
 const NewProdRow = ({ setProds }) => {
   const [prodName, setProdName] = useState("");
   const [prodPrice, setProdPrice] = useState("");
-  const nameRef = useRef(null);
-  const priceRef = useRef(null);
 
   const onProdAdd = async () => {
-    setProds((p) => [
-      ...p,
-      {
-        name: prodName,
-        amount: prodPrice,
-        id: uuidv4(),
-      },
-    ]);
-    setProdName();
-    setProdPrice();
+    if (prodName && prodPrice) {
+      setProds((p) => [
+        ...p,
+        {
+          name: prodName,
+          amount: prodPrice,
+          id: uuidv4(),
+        },
+      ]);
+      setProdName();
+      setProdPrice();
+    }
   };
+
+  const { NameInput, PriceInput } = useProdInputs({
+    nameProps: {
+      value: prodName,
+      onIonChange: (e) => setProdName(e.detail.value),
+      placeholder: "Nome prodotto",
+    },
+    priceProps: {
+      value: prodPrice,
+      style: { flex: 0.4 },
+      type: "number",
+      onIonChange: (e) => setProdPrice(e.detail.value),
+      placeholder: "Prezzo",
+    },
+    submit: onProdAdd,
+  });
 
   return (
     <CustomIonItem>
@@ -34,31 +51,8 @@ const NewProdRow = ({ setProds }) => {
       >
         <IonIcon icon={add} />
       </IonButton>
-      <IonInput
-        ref={nameRef}
-        onKeyPress={(e) => e.key === "Enter" && priceRef.current.setFocus()}
-        autocorrect
-        autocapitalize
-        value={prodName}
-        placeholder="Nome prodotto"
-        onIonChange={(e) => setProdName(e.detail.value)}
-        clearInput
-      ></IonInput>
-      <IonInput
-        ref={priceRef}
-        style={{ flex: 0.4 }}
-        onKeyPress={(e) => {
-          if (e.key === "Enter" && prodName && prodPrice) {
-            onProdAdd();
-            nameRef.current.setFocus();
-          }
-        }}
-        value={prodPrice}
-        type="number"
-        onIonChange={(e) => setProdPrice(e.detail.value)}
-        placeholder="Prezzo"
-        clearInput
-      ></IonInput>
+      {NameInput}
+      {PriceInput}
     </CustomIonItem>
   );
 };
