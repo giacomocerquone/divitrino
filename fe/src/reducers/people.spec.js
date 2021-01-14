5c9f8202bd14312f4566494f87e1f88ef2cc7658
Giacomo Cerquone <cerquone96@hotmail.it>
2021-01-14 15:46:58 +0100

    refactor: add people reducer tests

---
+++ fe/src/reducers/people.spec.js
@@ -0,0 +1,43 @@
+import peopleSlice, * as fromPeople from "./people";
+
+describe("people reducer", () => {
+  it("should return initial state on first run", () => {
+    const nextState = {
+      ids: [],
+      byId: {},
+    };
+
+    const result = peopleSlice.reducer(undefined, {});
+
+    expect(result).toEqual(nextState);
+  });
+
+  it("should correctly: add a person, get the people, get one person by id", () => {
+    const samplePerson = { id: "id", name: "me" };
+
+    const peopleState = peopleSlice.reducer(
+      undefined,
+      peopleSlice.actions.addPerson(samplePerson)
+    );
+
+    expect(fromPeople.getPersonById(peopleState, "id")).toEqual(samplePerson);
+    expect(fromPeople.getPeople(peopleState)).toEqual([samplePerson]);
+  });
+
+  it("should correctly delete a person", () => {
+    const movState = {
+      ids: ["id"],
+      byId: {
+        id: { id: "id", name: "me" },
+      },
+    };
+
+    const peopleState = peopleSlice.reducer(
+      movState,
+      peopleSlice.actions.delPerson("id")
+    );
+
+    expect(fromPeople.getPersonById(peopleState, "id")).toBe(undefined);
+    expect(fromPeople.getPeople(peopleState)).toEqual([]);
+  });
+});
