10a84900a816df1dd9cb5947971467b1eb8efb5a
Giacomo Cerquone <cerquone96@hotmail.it>
2021-02-17 16:54:50 +0100

    feat: update cropper version, preloading at boot

--- fe/src/App.js
+++ fe/src/App.js
@@ -6,18 +6,21 @@ import { store, persistor } from "store/store";
 import { PersistGate } from "redux-persist/integration/react";
 import AppRoutes from "App.routes";
 import GenericAlert from "components/atoms/GenericAlert";
+import { OpenCvProvider } from "opencv-react";
 
 const App = () => (
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <IonApp>
-        <IonReactRouter>
-          <AppRoutes />
-          <GenericAlert />
-        </IonReactRouter>
-      </IonApp>
-    </PersistGate>
-  </Provider>
+  <OpenCvProvider openCvPath="./opencv/opencv.js">
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <IonApp>
+          <IonReactRouter>
+            <AppRoutes />
+            <GenericAlert />
+          </IonReactRouter>
+        </IonApp>
+      </PersistGate>
+    </Provider>
+  </OpenCvProvider>
 );
 
 export default App;
