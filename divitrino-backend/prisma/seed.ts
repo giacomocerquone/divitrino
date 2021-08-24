17289b99da249eec5faaf009c0f5286fd82ad93e
giacomocerquone <cerquone96@hotmail.it>
2021-08-24 02:19:09 +0200

    feat(be): add pgsql volume for production

--- divitrino-backend/prisma/seed.ts
+++ divitrino-backend/prisma/seed.ts
@@ -18,88 +18,91 @@ const userData: Prisma.UserCreateInput[] = [
 
 async function main() {
   console.log(`Start seeding ...`);
-
-  const group = await prisma.group.create({
-    data: {
-      name: "Gruppo 1",
-    },
-  });
-  console.log(`Created group with id: ${group.id}`);
-  const createdUsers = [];
-
-  for (const u of userData) {
-    const user = await prisma.user.create({
+  try {
+    const group = await prisma.group.create({
       data: {
-        ...u,
-        groups: {
-          connect: {
-            id: group.id,
+        name: "Gruppo 1",
+      },
+    });
+    console.log(`Created group with id: ${group.id}`);
+    const createdUsers = [];
+
+    for (const u of userData) {
+      const user = await prisma.user.create({
+        data: {
+          ...u,
+          groups: {
+            connect: {
+              id: group.id,
+            },
           },
+        },
+      });
+      createdUsers.push(user);
+      console.log(
+        `Created user with id: ${user.id} attached to group with id: ${group.id}`
+      );
+    }
+
+    const purchase = await prisma.purchase.create({
+      data: {
+        payerId: createdUsers?.[0]?.id,
+        amount: 250,
+        groupId: group.id,
+        description: "Spesa conad pagata da Giacomo",
+        createdAt: new Date(),
+        products: {
+          create: [
+            {
+              name: "Prodotto diviso in due",
+              pricePerDebtor: 50,
+              debtors: {
+                connect: [
+                  {
+                    id: createdUsers?.[0]?.id,
+                  },
+                  {
+                    id: createdUsers?.[1]?.id,
+                  },
+                ],
+              },
+            },
+            {
+              name: "Prodotto di Giacomo",
+              pricePerDebtor: 100,
+              debtors: {
+                connect: [
+                  {
+                    id: createdUsers?.[0]?.id,
+                  },
+                ],
+              },
+            },
+            {
+              name: "Prodotto di Giovanni",
+              pricePerDebtor: 100,
+              debtors: {
+                connect: [
+                  {
+                    id: createdUsers?.[1]?.id,
+                  },
+                ],
+              },
+            },
+          ],
         },
       },
     });
-    createdUsers.push(user);
+
     console.log(
-      `Created user with id: ${user.id} attached to group with id: ${group.id}`
+      `Purchase payed by user with id ${createdUsers?.[0]?.id} in group with id ${group.id} which contains three product: one divided, 
+  one bought from user 1, one bought from user 2`
     );
+
+    console.log(`Seeding finished.`);
+  } catch (e) {
+    console.log("Seeding errored", e);
   }
-
-  const purchase = await prisma.purchase.create({
-    data: {
-      payerId: createdUsers?.[0]?.id,
-      amount: 250,
-      groupId: group.id,
-      description: "Spesa conad pagata da Giacomo",
-      createdAt: new Date(),
-      products: {
-        create: [
-          {
-            name: "Prodotto diviso in due",
-            pricePerDebtor: 50,
-            debtors: {
-              connect: [
-                {
-                  id: createdUsers?.[0]?.id,
-                },
-                {
-                  id: createdUsers?.[1]?.id,
-                },
-              ],
-            },
-          },
-          {
-            name: "Prodotto di Giacomo",
-            pricePerDebtor: 100,
-            debtors: {
-              connect: [
-                {
-                  id: createdUsers?.[0]?.id,
-                },
-              ],
-            },
-          },
-          {
-            name: "Prodotto di Giovanni",
-            pricePerDebtor: 100,
-            debtors: {
-              connect: [
-                {
-                  id: createdUsers?.[1]?.id,
-                },
-              ],
-            },
-          },
-        ],
-      },
-    },
-  });
-
-  console.log(
-    `Purchase payed by user with id ${createdUsers?.[0]?.id} in group with id ${group.id} which contains three product: one divided, 
-one bought from user 1, one bought from user 2`
-  );
-
-  console.log(`Seeding finished.`);
 }
 
 main()
