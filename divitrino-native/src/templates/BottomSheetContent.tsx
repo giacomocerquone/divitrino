da22f8da3f414da477b16a12ff0fe6ff65234cbf
giacomocerquone <cerquone96@hotmail.it>
2021-09-15 23:02:07 +0200

    feat(native): add bottom padding to bottomsheetcontent

--- divitrino-native/src/templates/BottomSheetContent.tsx
+++ divitrino-native/src/templates/BottomSheetContent.tsx
@@ -1,6 +1,7 @@
 import { useBottomSheetModal } from "@gorhom/bottom-sheet";
 import React, { FunctionComponent, ReactNode } from "react";
 import { StyleSheet, View, ViewStyle } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import BottomSheetHeader from "../components/organisms/BottomSheetHeader";
 import { colors, unit } from "../constants/ui";
@@ -16,11 +17,16 @@ const BottomSheetContent: FunctionComponent<Props> = ({
     dismissAll();
     return true;
   });
+  const insets = useSafeAreaInsets();
 
   return (
     <View style={styles.root}>
       <BottomSheetHeader title={headerTitle} />
-      <View style={contentContainerStyle}>{children}</View>
+      <View
+        style={[{ paddingBottom: insets.bottom * 10 }, contentContainerStyle]}
+      >
+        {children}
+      </View>
     </View>
   );
 };
