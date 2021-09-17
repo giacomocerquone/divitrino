cba2c3d2de1fa7d8ded18c057cf8d06c0d8d7c8d
Giacomo Cerquone <cerquone96@hotmail.it>
2021-09-17 09:49:43 +0200

    update migration to avoid losing data

--- divitrino-backend/prisma/migrations/20210917065656_new_movement_structure/migration.sql
+++ divitrino-backend/prisma/migrations/20210917065656_new_movement_structure/migration.sql
@@ -7,39 +7,6 @@
   - Added the required column `movementId` to the `Product` table without a default value. This is not possible if the table is not empty.
 
 */
--- DropForeignKey
-ALTER TABLE "Payment" DROP CONSTRAINT "Payment_addedByUserId_fkey";
-
--- DropForeignKey
-ALTER TABLE "Payment" DROP CONSTRAINT "Payment_groupId_fkey";
-
--- DropForeignKey
-ALTER TABLE "Payment" DROP CONSTRAINT "Payment_payeeId_fkey";
-
--- DropForeignKey
-ALTER TABLE "Payment" DROP CONSTRAINT "Payment_payerId_fkey";
-
--- DropForeignKey
-ALTER TABLE "Product" DROP CONSTRAINT "Product_purchaseId_fkey";
-
--- DropForeignKey
-ALTER TABLE "Purchase" DROP CONSTRAINT "Purchase_addedByUserId_fkey";
-
--- DropForeignKey
-ALTER TABLE "Purchase" DROP CONSTRAINT "Purchase_groupId_fkey";
-
--- DropForeignKey
-ALTER TABLE "Purchase" DROP CONSTRAINT "Purchase_payerId_fkey";
-
--- AlterTable
-ALTER TABLE "Product" DROP COLUMN "purchaseId",
-ADD COLUMN     "movementId" TEXT NOT NULL;
-
--- DropTable
-DROP TABLE "Payment";
-
--- DropTable
-DROP TABLE "Purchase";
 
 -- CreateTable
 CREATE TABLE "Movement" (
@@ -69,5 +36,43 @@ ALTER TABLE "Movement" ADD FOREIGN KEY ("payerId") REFERENCES "User"("id") ON DE
 -- AddForeignKey
 ALTER TABLE "Movement" ADD FOREIGN KEY ("payeeId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
--- AddForeignKey
-ALTER TABLE "Product" ADD FOREIGN KEY ("movementId") REFERENCES "Movement"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+INSERT INTO "Movement" ("createdAt", "updatedAt", "id", "description", "date", "addedByUserId", "groupId", "payerId") SELECT "createdAt", "updatedAt", "id", "description", "date", "addedByUserId", "groupId", "payerId" FROM "Purchase";
+
+INSERT INTO "Movement" ("createdAt", "updatedAt", "id", "amount", "date", "addedByUserId", "groupId", "payerId", "payeeId") SELECT "createdAt", "updatedAt", "id", "amount", "date", "addedByUserId", "groupId", "payerId", "payeeId" FROM "Payment";
+
+-- DropForeignKey
+ALTER TABLE "Payment" DROP CONSTRAINT "Payment_addedByUserId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Payment" DROP CONSTRAINT "Payment_groupId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Payment" DROP CONSTRAINT "Payment_payeeId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Payment" DROP CONSTRAINT "Payment_payerId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Product" DROP CONSTRAINT "Product_purchaseId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Purchase" DROP CONSTRAINT "Purchase_addedByUserId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Purchase" DROP CONSTRAINT "Purchase_groupId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Purchase" DROP CONSTRAINT "Purchase_payerId_fkey";
+
+-- DropTable
+DROP TABLE "Payment";
+
+-- DropTable
+DROP TABLE "Purchase";
+
+-- AlterTable
+
+ALTER TABLE "Product" RENAME COLUMN "purchaseId" TO "movementId";
+
+ALTER TABLE "Product" ADD CONSTRAINT "Product_movementId_fkey" FOREIGN KEY ("movementId") REFERENCES "Movement"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
